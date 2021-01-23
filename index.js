f3c85562803e4c97f0740404ae8b730668690f50
erikn775 <erikn775@gmail.com>
2021-01-23 11:31:36 -0700

    commit tree 9d1de0b3b80ba434634ddc9b27c39b83a211c36d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(count) {
+
+    for (let i = count; i >= 0; i--  ) {
+        console.log(i)
+    }
+}
+
+const cards = ['Lisa', 'Kaitlin', 'Jan'];
+let newArray = []
+function writeCards(cards) {
+
+    for (let i = 0; i < cards.length; i++) {
+        newArray.push(`Thank you, ${cards[i]}, for the wonderful suprise gift!`);
+    }
+    return newArray;
+}
